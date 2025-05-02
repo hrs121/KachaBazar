689c4f6f92821872c8df5c4f288c8b0e0520e1b1
Md Humayun Rashid <92227819+hrs121@users.noreply.github.com>
2025-05-03 00:52:44 +0600

    initial commit

---
+++ kachabazar-frontend/app/page.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import Header from '../Components/Header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-center text-4xl my-8">Welcome to Our Shop</h1>
+    </div>
+  );
+};
+
+export default HomePage;
