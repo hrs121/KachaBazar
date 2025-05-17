d8bf348af296592da6d65ee383a572a22aa73954
EftekharAlam2 <eftekharul@ba-systems.com>
2025-05-17 07:25:21 +0600

    login and registration routes set up

--- kachabazar-backend/models/users.js
+++ kachabazar-backend/models/users.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  description: String
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  mobile: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', UserSchema);
